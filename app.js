d11f1d7e755629f2604122896577ddf69290ad53
sadik126 <sadikhimel04@gmail.com>
2022-02-18 08:23:45 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//create onload handeler
+
+//random rgb color genetor function
+
+//collect all references
+
+
+//handle click event
+
+window.onload = function load(params) {
+    main();
+}
+
+function main(){
+    
+}
