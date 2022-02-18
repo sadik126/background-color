30a570940ea5f040d16d62c9efac9eab08f0a961
sadik126 <sadikhimel04@gmail.com>
2022-02-18 08:34:49 +0600

    m

--- app.js
+++ app.js
@@ -12,5 +12,12 @@ window.onload = function load(params) {
 }
 
 function main(){
-    
+
+}
+
+function rgbcolor(){
+    let red = Math.round(Math.random()*255) ;
+    let blue =  Math.round(Math.random()*255);
+
+    let green =  Math.round(Math.random()*255);
 }
